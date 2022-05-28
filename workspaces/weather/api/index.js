998913ab3f8945d4ef0ed68830e8ab54997042da
ashalfarhan <ashalfarhan8@gmail.com>
2022-05-28 09:58:32 +0700

    fix: change api

--- workspaces/weather/api/index.js
+++ workspaces/weather/api/index.js
@@ -1,10 +1,14 @@
+// @ts-check
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
 export default createProxyMiddleware({
-  target: 'https://www.metaweather.com/api',
+  target: 'http://api.weatherapi.com/v1',
   changeOrigin: true,
   logLevel: 'debug',
-  pathRewrite: {
-    '^/api': '',
+  pathRewrite: path => path.replace(/^\/api/, ''),
+  onProxyReq: proxyReq => {
+    const parsed = new URL(proxyReq.path, 'http://api.weatherapi.com/v1');
+    parsed.searchParams.set('key', process.env.WEATHER_API_KEY);
+    proxyReq.path = parsed.pathname + parsed.search;
   },
 });
